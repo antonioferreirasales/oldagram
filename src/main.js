abc27126ff44dc8b746cec58eb4dc2403a12ac21
Antônio Sales <antoniosales1003@gmail.com>
2023-09-22 17:31:22 -0300

    fix: refactor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { posts } from './posts'
 
 const postContentEl = document.querySelector(".post-content")
+const giveLikeEl = document.querySelector("#give-like")
+renderPosts()
+console.log(giveLikeEl)
+// giveLikeEl.addEventListener("dblclick", function() {
+//   console.log("click")
+// } )
+
+function image() {
+  console.log("click")
+}
 
 function renderPosts () {
   for (let i = 0; i < posts.length; i++){
@@ -15,11 +25,11 @@ function renderPosts () {
           </div>
         </div>
         <div class="post-image">
-          <img class="post-image" src=${profile.post} alt="post image">
+          <img class="post-image give-like" src=${profile.post} alt="post image" ondblclick="image()">
         </div>
         <div class="post-footer">
           <div class="post-icons">
-            <img class="icons" src="img/icon-heart.png" alt="heart icon">
+            <img class="icons give-like" src="img/icon-heart.png" alt="heart icon">
             <img class="icons" src="img/icon-dm.png" alt="dm icon">
             <img class="icons" src="img/icon-comment.png" alt="comment icon">
           </div>
@@ -31,4 +41,3 @@ function renderPosts () {
   }
 }
 
-renderPosts()

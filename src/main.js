0a2821e73ccbd0cb0c6a388c81d8e1dab535739d
Antônio Sales <antoniosales1003@gmail.com>
2023-12-18 17:31:29 -0300

    feat: add like button function

--- src/main.js
+++ src/main.js
@@ -1,20 +1,39 @@
-import { posts } from './posts'
+import { posts } from "./posts";
 
-const postContentEl = document.querySelector(".post-content")
-const giveLikeEl = document.querySelector("#give-like")
-renderPosts()
-console.log(giveLikeEl)
-// giveLikeEl.addEventListener("dblclick", function() {
-//   console.log("click")
-// } )
+const postContentEl = document.querySelector(".post-content");
 
-function image() {
-  console.log("click")
-}
+// aguardar a criação do DOM para pegar o conjunto de elementos com a classe "give-like"
+document.addEventListener("DOMContentLoaded", (event) => {
+  const giveLikeEl = document.querySelectorAll(".give-like");
+  giveLikeEl.forEach((giveLikeEl) => {
+    giveLikeEl.addEventListener("dblclick", (giveLikeEl) => {
+      // pega o atributo alt do elemento e retorna a última palavra contendo o usuário.
+      const words = giveLikeEl.target.alt.split(" ").pop();
+      // console.log(words);
+      posts.forEach((post) => {
+        if (post.username === words) {
+          post.likes += 1;
+          const likes = document.querySelectorAll(".likes");
+          likes.forEach((element) => {
+            if (element.id === words) {
+              element.innerHTML = `${post.likes} likes`;
+            }
+          });
+        }
+      });
+    });
+  });
+});
 
-function renderPosts () {
-  for (let i = 0; i < posts.length; i++){
-    let profile = posts[i]
+renderPosts();
+
+// giveLikeEl.addEventListener("dblclick", function () {
+//   console.log("click");
+// });
+
+function renderPosts() {
+  for (let i = 0; i < posts.length; i++) {
+    let profile = posts[i];
     postContentEl.innerHTML += `
     <div class="container">
         <div class="post-header">
@@ -25,19 +44,18 @@ function renderPosts () {
           </div>
         </div>
         <div class="post-image">
-          <img class="post-image give-like" src=${profile.post} alt="post image" ondblclick="image()">
+          <img class="post-image give-like" src=${profile.post} alt="post image of ${profile.username}">
         </div>
         <div class="post-footer">
           <div class="post-icons">
-            <img class="icons give-like" src="img/icon-heart.png" alt="heart icon">
+            <img class="icons give-like" src="img/icon-heart.png" alt="heart icon of ${profile.username}">
             <img class="icons" src="img/icon-dm.png" alt="dm icon">
             <img class="icons" src="img/icon-comment.png" alt="comment icon">
           </div>
-          <span class="text-callout comment">${profile.likes} likes</span>
+          <span id="${profile.username}" class="text-callout comment likes">${profile.likes} likes</span>
           <p class="comment"><span class="text-callout">${profile.username}</span>${profile.comment}</p>
         </div>
     </div>
-  `
+  `;
   }
 }
-
--- src/posts.js
+++ src/posts.js
@@ -1,29 +1,30 @@
 export const posts = [
   {
-      name: "Vincent van Gogh",
-      username: "vincey1853",
-      location: "Zundert, Netherlands",
-      avatar: "img/avatar-vangogh.jpg",
-      post: "img/post-vangogh.jpg",
-      comment: "just took a few mushrooms lol",
-      likes: 21
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "img/avatar-vangogh.jpg",
+    post: "img/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
   },
   {
-      name: "Gustave Courbet",
-      username: "gus1819",
-      location: "Ornans, France",
-      avatar: "img/avatar-courbet.jpg",
-      post: "img/post-courbet.jpg",
-      comment: "i'm feelin a bit stressed tbh",
-      likes: 4
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "img/avatar-courbet.jpg",
+    post: "img/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
   },
-      {
-      name: "Joseph Ducreux",
-      username: "jd1735",
-      location: "Paris, France",
-      avatar: "img/avatar-ducreux.jpg",
-      post: "img/post-ducreux.jpg",
-      comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-      likes: 152
-  }
-]
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "img/avatar-ducreux.jpg",
+    post: "img/post-ducreux.jpg",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+  },
+];
